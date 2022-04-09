4368828cd07bc21a1a3afd9619bda494a90f1429
Mert Guner <mertcannguner@gmail.com>
2022-04-09 18:02:19 +0300

    mvc

---
+++ PersonelMvc/PersonelMvc/Controllers/DepartmanController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PersonelMvc.Models.EntityFramework;
+
+namespace PersonelMvc.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+
+        PersonelMvcEntities db = new PersonelMvcEntities(); // bütün database bilgilerimi db ye aldım
+
+        public ActionResult Index()
+        {
+            var model = db.Departman.ToList(); // hepsini getir
+            return View(model); // model ı wiew e gönderiyoruz
+        }
+
+        [HttpGet] // veriyi aldığı kısım okuduğu kısım
+        public ActionResult Yeni()
+        {
+            return View("DepartmanForm"); // view ile metodun adı illa aynı olmak zorunda değil
+        }
+
+        [HttpPost] // veriyi yazıyoruz  // (FormMethod.Post basıldığında bu alan çalışır)
+        public ActionResult Kaydet(Departman departman)
+        {
+            if (departman.Id == 0)  // id 0 ise yeni kayıt
+            {
+                db.Departman.Add(departman);
+            }
+            else  // idsi olan kayıdı güncelle (değilse var olan kayıdı güncelle)
+            {
+                var guncellenecekdepartman = db.Departman.Find(departman.Id);
+
+                if (guncellenecekdepartman == null)
+                {
+                    return HttpNotFound();
+                }
+
+                guncellenecekdepartman.Ad = departman.Ad;
+            }
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index","Departman"); // işlem olunca departman controller da ki Index e gönder (form_load gibi)
+        }
+
+
+
+        public ActionResult Guncelle(int id)
+        {
+            var model = db.Departman.Find(id);
+
+            if (model == null)
+            {
+                return HttpNotFound();
+                
+            }
+
+            
+
+            return View("DepartmanForm", model);
+        }
+
+
+
+        public ActionResult Sil(int id)
+        {
+            var silinicekdepartman = db.Departman.Find(id);
+
+            if (silinicekdepartman == null)
+            {
+                return HttpNotFound();
+            }
+
+            db.Departman.Remove(silinicekdepartman);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PersonelMvc/PersonelMvc/Controllers/PersonelController.cs
@@ -0,0 +1,85 @@
+﻿using PersonelMvc.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using PersonelMvc.ViewModels;
+
+
+namespace PersonelMvc.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        PersonelMvcEntities db = new PersonelMvcEntities();
+
+        
+  
+        public ActionResult Index()
+        {
+            var model = db.Personel.Include(x => x.Departman).ToList();
+            
+            return View(model);
+        }
+
+        public ActionResult Yeni()
+        {
+            var model = new PersonelFormViewModel()
+            {
+                Departmanlar = db.Departman.ToList()
+            };
+
+            return View("PersonelForm", model);
+            
+        }
+
+        public ActionResult Kaydet(Personel personel)
+        {
+            if (personel.Id == 0)
+            {
+                db.Personel.Add(personel);
+            }
+            else
+            {
+                // bu satırda id bilgisini almadan o an ki kayıdın güncellenmesini sağlıyoruz.
+                db.Entry(personel).State = System.Data.Entity.EntityState.Modified;
+            }
+
+            // kayıt işlemi yapılıyor
+            db.SaveChanges();
+
+            // index sayfasına yönlendiriyoruz
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Guncelle(int id)
+        {
+            var model = new PersonelFormViewModel()
+            {
+                Departmanlar = db.Departman.ToList(),
+                Personel = db.Personel.Find(id)
+
+            };
+
+            return View("PersonelForm", model);
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var silinecekpersonel = db.Personel.Find(id);
+
+            if (silinecekpersonel == null)
+            {
+                return HttpNotFound();
+            }
+
+            db.Personel.Remove(silinecekpersonel);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
